601d708c7b2c299c48a157025d9e1e81e3b5a55f
Shekhar Dhangar <shekhardhangar@yahoo.com>
2021-08-23 21:54:49 +0530

    test: added new tests for input checking

--- index.js
+++ index.js
@@ -3,29 +3,32 @@ const luckyNumber = document.querySelector(".lucky-number")
 const checkBtn = document.querySelector(".btn-check");
 const output = document.querySelector(".output-para");
 
-checkBtn.addEventListener("click",sumOfDOB )
-
 function sumOfDOB(){
     
     const dob = dateOfBirth.value;
+    if(luckyNumber.value==""){
+        output.innerText = "Please enter a number";
+    }else{
     const finaldate = dob.replaceAll("-","");
         let sum = 0;
         for(var i = 0;i<finaldate.length;i++){
             sum += Number(finaldate[i])
         }
-
         if(sum && dob)
         calculateAns(sum,luckyNumber.value);
         else
         output.innerText = "Enter both fields";
-        
+    }
 }
 
 function calculateAns(sum,luckyNumber){
+
     console.log(sum, luckyNumber)
     if(sum%luckyNumber === 0){
-        output.innerText =  luckyNumber + " is a Lucky number!!🎉🎉";
+        output.innerText =  "Your birthday is lucky!!🎉🎉";
     }else{
-        output.innerText = luckyNumber +" is not a Lucky Number😞";
+        output.innerText = "Your birthday is not lucky😞";
     }
 }
+
+checkBtn.addEventListener("click",sumOfDOB );
